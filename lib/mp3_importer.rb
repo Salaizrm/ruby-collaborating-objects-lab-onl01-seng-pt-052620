6c88bbfb622e6d9257f383991fcf090d136f0866
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-26 21:20:55 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,12 +7,18 @@ class MP3Importer
   end
 
   def files
-    @files
+  files = []
+  Dir.new(self.path).each do |file|
+    files << file if file.length > 4
   end
+  files
+end
 
-  def import(files)
-    files.each { |filename| Song.files(filename) }
+def import
+  self.files.each do |filename|
+    Song.new_by_filename(filename)
   end
+end
 
 
 end
