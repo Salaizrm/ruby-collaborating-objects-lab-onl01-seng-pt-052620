6197fcb24f5f2e8b796a5cf620339f5881363998
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-26 21:11:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,11 +14,14 @@ class Song
     @@all
   end
 
+
+
   def self.new_by_filename(filename)
-    song = self.new
-    song.artist = filename.split(" - ")[1]
-    song
-  end
+  artist, song = filename.split(" - ")
+  new_song = self.new(song)
+  new_song.artist_name = artist
+  new_song
+end
 
 
 end
