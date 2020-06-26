9987b1223c302fa63d32dcded362e90890011d2e
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-26 21:12:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,14 @@
 class Artist
-
-  attr_accessor :name
-
   @@all = []
+  attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
 
   def self.all
-    @@all
+    @@all    
   end
 
   def add_song(song)
@@ -18,13 +16,22 @@ class Artist
   end
 
   def songs
-    Song.all.select{|song| song.artist == self}
+    Song.all.select {|song| song.artist == self}
   end
 
   def self.find_or_create_by_name(name)
     self.find(name) ? self.find(name) : self.new(name)
   end
 
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
 
+  def save
+    @@all << self
+  end
 
+  def print_songs
+    songs.each {|song| puts song.name}
+  end
 end
